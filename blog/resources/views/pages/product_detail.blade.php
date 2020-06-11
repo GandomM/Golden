827107178defcf0cde77b30f9c49570a2fab7275
Masomeh <gandoompakdel@gmail.com>
2020-06-11 05:57:13 +0430

    Update product_detail.blade.php

--- blog/resources/views/pages/product_detail.blade.php
+++ blog/resources/views/pages/product_detail.blade.php
@@ -4,6 +4,7 @@
  <div class="page-content">
             <div class="container">
                 <div class="row">
+                   
                     <div class="col-md-6 col-sm-6">
                         <div class="simple-gallery">
                             <img class="gallery-main-image triggerAnimation animated img-responsive" data-animate='fadeIn' 
@@ -12,24 +13,25 @@
 
                             <ul class="gallery-thumbnails triggerAnimation animated" data-animate='fadeIn'>
                                 <li class="gallery-thumb">
-                                    <img src="" class="img-responsive" alt="Construction HTML Template" width="555" height="333" />
+                                    <img src="{{'images/'.$product_details->image}}" class="img-responsive" alt="Construction HTML Template" width="555" height="333" />
                                     <div class="hover-overlay"></div>
                                 </li>
                                 <li class="gallery-thumb">
-                                    <img src="" class="img-responsive" alt="Construction HTML Template" width="555" height="333" />
+                                    <img src="{{'images/'.$product_details->image}}" class="img-responsive" alt="Construction HTML Template" width="555" height="333" />
                                     <div class="hover-overlay"></div>
                                 </li>
                                 <li class="gallery-thumb">
-                                    <img src="" class="img-responsive" alt="Construction HTML Template" width="555" height="333" />
+                                    <img src="{{'images/'.$product_details->image}}" class="img-responsive" alt="Construction HTML Template" width="555" height="333" />
                                     <div class="hover-overlay"></div>
                                 </li>
                             </ul>
                         </div>   
                     </div><!-- .col-md-6 end -->
+                    @endforeach
 <!--ترجمه شده توسط مرجع تخصصی برنامه نویسان-->
                     <div class="col-md-6 col-sm-6">
                         <h1>
-                            {{$project_details->project_name}}
+                            {{$product_details->product_name}}
                         </h1>
                         <div class="custom-heading style-1">
                             <h2>
@@ -48,8 +50,7 @@
                            
 
                             
-
-{{$project_details->owner}}  </li>
+  </li>
                             <li>
                                 دیزاین:
 
@@ -62,27 +63,13 @@
                             </li>
                             <li>
                                 ضخامت:
-{{$project_details->technis}}  
+{{$product_details->technis}}  
 
                             </li>
-                               <li>
-                                مواد آلمونیم:
-{{$project_details->matrial}}  
-
-                            </li>
-                               <li>
-                                موقعیت:
-{{$project_details->location}}  
-
-                            </li>
-                               <li>
-                                شهر:
-{{$project_details->city}}  
-
-                            </li>
+            
                                <li>
                                 تاریخ:
-{{$project_details->date}}  
+{{$product_details->detail}}  
 
                             </li>
                         </ul>
